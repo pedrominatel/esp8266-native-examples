aa49e662167c836efc7e8868810855810bcd354b
pedrominatel <pminatel@gmail.com>
2016-01-09 14:17:15 -0200

    folders renamed and new project added

--- blinky/user/user_main.c
+++ blinky/user/user_main.c
@@ -4,26 +4,31 @@
 #include "os_type.h"
 #include "user_config.h"
 
-#define user_procTaskPrio        0
-#define user_procTaskQueueLen    1
+#define user_procTaskPrio		0
+#define user_procTaskQueueLen	1
+
+//GPIO a ser utilizada
+#define GPIO					BIT5
+
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void user_procTask(os_event_t *events);
 
+//Definição do TIMER a ser utilizado
 static volatile os_timer_t some_timer;
 
-
+/* Função de blink
+ * */
 void some_timerfunc(void *arg)
 {
-    //Do blinky stuff
-    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & BIT2)
+    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & GPIO)
     {
-        //Set GPIO2 to LOW
-        gpio_output_set(0, BIT2, BIT2, 0);
+        //GPIO LOW
+    	GPIO_OUTPUT_SET(5,0);
     }
     else
     {
-        //Set GPIO2 to HIGH
-        gpio_output_set(BIT2, 0, BIT2, 0);
+        //GPIO HIGH
+    	GPIO_OUTPUT_SET(5,1);
     }
 }
 
@@ -38,27 +43,18 @@ user_procTask(os_event_t *events)
 void ICACHE_FLASH_ATTR
 user_init()
 {
-    // Initialize the GPIO subsystem.
-    gpio_init();
+    //Configura o modo da GPIO
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
+    //Configura a GPIO para OUTPUT e define para LOW LEVEL
+    GPIO_OUTPUT_SET(GPIO_ID_PIN(5), 1);
 
-    //Set GPIO2 to output mode
-    PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-
-    //Set GPIO2 low
-    gpio_output_set(0, BIT2, BIT2, 0);
-
-    //Disarm timer
+    //Desabilita o timer para a confuguração
     os_timer_disarm(&some_timer);
-
-    //Setup timer
+    //Configura o TIMER
     os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, NULL);
-
-    //Arm the timer
-    //&some_timer is the pointer
-    //1000 is the fire time in ms
-    //0 for once and 1 for repeating
+    //Arma o timer
     os_timer_arm(&some_timer, 1000, 1);
     
-    //Start os task
-    system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
+    //Inicia a uma tarefa no sistema
+    //system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 }
---
+++ web-server/user/user_main.c
@@ -0,0 +1,6 @@
+#include "osapi.h"
+#include "os_type.h"
+#include <user_interface.h>
+
+void user_init(void) {
+}
