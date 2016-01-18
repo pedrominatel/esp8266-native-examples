e66fd34937cd5e353a7523df103a784c20affdcc
pedrominatel <pminatel@gmail.com>
2016-01-18 21:34:50 -0200

    added sht21 example - not finished

---
+++ sensor-sht21/include/wifi_manager.h
@@ -0,0 +1,19 @@
+/*
+ * wifi_manager.h
+ *
+ *  Created on: Jan 9, 2016
+ *      Author: piiiters
+ */
+
+#ifndef INCLUDE_WIFI_MANAGER_H_
+#define INCLUDE_WIFI_MANAGER_H_
+
+#include "osapi.h"
+#include "os_type.h"
+#include <user_interface.h>
+
+void ICACHE_FLASH_ATTR wifi_inti(void);
+//void ICACHE_FLASH_ATTR wifi_setup(char* ssid, char* password, uint8_t mode);
+void ICACHE_FLASH_ATTR wifiScan_cb(void *arg, STATUS status);
+
+#endif /* INCLUDE_WIFI_MANAGER_H_ */
