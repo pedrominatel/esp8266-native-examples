337702d5751686e92a87e2f627b4039ab8b50d6e
pedrominatel <pminatel@gmail.com>
2016-01-20 18:50:36 -0200

    blink-os-timer bug fixed

--- blink-os-timer/user/user_main.c
+++ blink-os-timer/user/user_main.c
@@ -1,85 +1,65 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2015 Pedro Minatel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
+#include "ets_sys.h"
 #include "osapi.h"
 #include "gpio.h"
 #include "os_type.h"
 #include "user_config.h"
- 
+
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
- 
-//GPIO a ser utilizada
-#define GPIO                    BIT5
- 
+
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void user_procTask(os_event_t *events);
- 
-//Definição do TIMER a ser utilizado
+
+//Define a GPIO que sera usada
+uint32_t gpio5 = BIT5;
+
 static volatile os_timer_t some_timer;
- 
-/* Função de blink
-* */
+
 void some_timerfunc(void *arg)
 {
-if (GPIO_REG_READ(GPIO_OUT_ADDRESS) &amp; GPIO)
-{
-//GPIO LOW
-GPIO_OUTPUT_SET(5,0);
+    //Do blinky stuff
+    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & gpio5)
+    {
+        //Set GPIO2 to LOW
+        gpio_output_set(0, gpio5, gpio5, 0);
+    }
+    else
+    {
+        //Set GPIO2 to HIGH
+        gpio_output_set(gpio5, 0, gpio5, 0);
+    }
 }
-else
-{
-//GPIO HIGH
-GPIO_OUTPUT_SET(5,1);
-}
-}
- 
-//Do nothing function
+
+//Função NOP (NO OPERATION)
 static void ICACHE_FLASH_ATTR
 user_procTask(os_event_t *events)
 {
-os_delay_us(10);
+    os_delay_us(10);
 }
- 
+
 //Init function
 void ICACHE_FLASH_ATTR
 user_init()
 {
-//Inicialização do modulo de GPIO
-gpio_init();
-//Configura o modo da GPIO
-PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
-//Configura a GPIO para OUTPUT e define para LOW LEVEL
-GPIO_OUTPUT_SET(GPIO_ID_PIN(5), 0);
- 
-//Desabilita o timer para a confuguração
-os_timer_disarm(&amp;some_timer);
-//Configura o TIMER
-os_timer_setfn(&amp;some_timer, (os_timer_func_t *)some_timerfunc, NULL);
-//Arma o timer
-os_timer_arm(&amp;some_timer, 1000, 1);
- 
-//Inicia a uma tarefa no sistema
-system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
+    //Inicialização do modulo de GPIO
+    gpio_init();
+    //Configura a GPIO5
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
+    //Set GPIO2 low
+    gpio_output_set(0, gpio5, gpio5, 0);
+
+    //Disarm timer
+    os_timer_disarm(&some_timer);
+
+    //Setup timer
+    os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, NULL);
+
+    //Arm the timer
+    //&some_timer is the pointer
+    //1000 is the fire time in ms
+    //0 for once and 1 for repeating
+    os_timer_arm(&some_timer, 1000, 1);
+
+    //Start os task
+    system_os_task(user_procTask, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 }
